0d7fd1323252d46fbd45fec6fbdb73cbb296dd5a
j-yong98 <chlwodyd9829@gmail.com>
2025-10-19 12:50:33 +0900

    BOJ32173

---
+++ j-yong98/BOJ32173.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ32173 {
+
+    static int N;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        long sum = 0;
+        long ans = 0;
+        for (int i = 0; i < N; i++) {
+            ans = Math.max(ans, arr[i] - sum);
+            sum += arr[i];
+        }
+        System.out.println(ans);
+    }
+}
